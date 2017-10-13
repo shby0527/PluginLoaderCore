e5bccae98fd335d68737a96a22c7e84d0b783238
Hayama Umi <umi@hayama.cf>
2017-10-13 14:53:31 +0800

    修改插件接口，添加加载器插件信息结构基类

---
+++ PluginLoader/Loader/Loader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginLoader.Loader
+{
+    public class Loader
+    {
+    }
+}
---
+++ PluginLoader/PluginCollection/IPluginCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+using PluginLoader.Plugin.Common;
+
+namespace PluginLoader.PluginCollection
+{
+    /// <summary>
+    /// 插件集合
+    /// </summary>
+    public interface IPluginCollection<out T> : IEnumerable<T>, IEnumerable
+        where T : class, IPlugin
+    {
+        T this[int index] { get; }
+
+        T this[string name] { get; }
+
+        uint Count { get; }
+    }
+}
