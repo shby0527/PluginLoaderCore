2b9d218061e36116c042b2d73042be2d3f307905
Hayama Umi <umi@hayama.cf>
2017-10-13 15:14:09 +0800

    完善完整插件信息

---
+++ PluginLoader/PluginInfo/LazyLoadingInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using PluginLoader.Plugin.Common;
+
+namespace PluginLoader.PluginInfo
+{
+    internal sealed class LazyLoadingInfo : PluginInfo
+    {
+        public LazyLoadingInfo(Type plugin, IPluginFactory factory)
+            : base(plugin, factory)
+        {
+        }
+
+        public override T CreateInstance<T>()
+        {
+            IPluginFactory factory = this.GetPluginFactory();
+            IPlugin plugin = factory.CreateInstance(this.PluginType);
+            if (plugin is T)
+                return (T)plugin;
+            return default(T);
+        }
+    }
+}
